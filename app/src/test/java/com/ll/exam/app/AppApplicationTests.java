ba2ffc25e6c639bcd43004e485ad1e1ddd623533
leewooyup <zjadlspun1114@naver.com>
2022-09-24 07:25:32 +0900

    mockMvc로 로그인 처리하여 테스트케이스 구현

--- app/src/test/java/com/ll/exam/app/AppApplicationTests.java
+++ app/src/test/java/com/ll/exam/app/AppApplicationTests.java
@@ -1,6 +1,7 @@
 package com.ll.exam.app;
 
 import com.ll.exam.app.home.controller.HomeController;
+import com.ll.exam.app.member.controller.MemberController;
 import com.ll.exam.app.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -50,9 +52,45 @@ class AppApplicationTests {
 
 	@Test
 	@DisplayName("회원의 수")
-	@Rollback(false)
 	void t2() throws Exception {
 		long count = memberService.count();
 		assertThat(count).isGreaterThan(0L);
+	}
+
+	@Test
+	@DisplayName("user1로 로그인 후, 프로필페이지에 접속하면 user1의 이메일이 보여야한다")
+	void t3() throws Exception {
+		//mockMvc로 로그인 처리
+		ResultActions resultActions = mvc
+				.perform(
+						get("/member/profile")
+								.with(user("user1").password("1234").roles("user"))
+				)
+				.andDo(print());
+
+		resultActions
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(MemberController.class))
+				.andExpect(handler().methodName("showProfile"))
+				.andExpect(content().string(containsString("user1@test.com")));
+
+	}
+
+	@Test
+	@DisplayName("user4로 로그인 후, 프로필페이지에 접속하면 user4의 이메일이 보여야한다")
+	void t4() throws Exception {
+		//mockMvc로 로그인 처리
+		ResultActions resultActions = mvc
+				.perform(
+						get("/member/profile")
+								.with(user("user4").password("1234").roles("user"))
+				)
+				.andDo(print());
+
+		resultActions
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(handler().handlerType(MemberController.class))
+				.andExpect(handler().methodName("showProfile"))
+				.andExpect(content().string(containsString("user4@test.com")));
 	}
 }
